5ac992c2c93f322ead1a1b29f418be2e59222e17
calvin4370 <calvin.j.j.chan@gmail.com>
2025-12-22 22:30:42 +0800

    Update 01_schema.sql
    - Added order_reviews tableau
    - Update orders table to add in remaining columns

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -3,7 +3,11 @@ CREATE TABLE orders (
     order_id VARCHAR(50) PRIMARY KEY,
     customer_id VARCHAR(50),
     order_status VARCHAR(50),
-    order_purchase_timestamp DATETIME
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME,
+    order_delivered_carrier_date DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME
 );
 
 DROP TABLE IF EXISTS order_items;
@@ -25,4 +29,16 @@ CREATE TABLE customers (
     customer_zip_code_prefix INT,
     customer_city VARCHAR(100),
     customer_state CHAR(2)
+);
+
+DROP TABLE IF EXISTS order_reviews;
+CREATE TABLE order_reviews (
+    review_id VARCHAR(50),
+    order_id VARCHAR(50),
+    review_score INT,
+    review_comment_title VARCHAR(255),
+    review_comment_message TEXT,
+    review_creation_date DATETIME,
+    review_answer_timestamp DATETIME
+    -- don't set a Primary Key here to avoid import errors 
 );
